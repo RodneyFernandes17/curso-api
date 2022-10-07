0fe2f150f49d6c06ed6a0c7e67c487e7e2c5b41f
RodneyFernandes17 <rodfelfer@hotmail.com>
2022-10-07 14:01:22 +0100

    projeto iniciado

---
+++ src/test/java/ausy/pt/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ausy.pt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
