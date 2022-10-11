adc8de3d7a2e042fee7cf508a91470ab02e721de
RodneyFernandes17 <rodfelfer@hotmail.com>
2022-10-11 13:09:18 +0100

    disponibilizando um model mapper em forma bean

--- src/main/java/ausy/pt/api/resources/UserResource.java
+++ src/main/java/ausy/pt/api/resources/UserResource.java
@@ -1,7 +1,8 @@
 package ausy.pt.api.resources;
 
-import ausy.pt.api.domain.User;
+import ausy.pt.api.domain.dto.UserDTO;
 import ausy.pt.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @RequestMapping(value="/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
