c781ebbf26faa35d33c9cf89c984e661e93111a7
RodneyFernandes17 <rodfelfer@hotmail.com>
2022-10-17 19:44:56 +0100

    teste para classe user service metodo delete com sucesso

--- src/test/java/ausy/pt/api/services/impl/UserServiceImplTest.java
+++ src/test/java/ausy/pt/api/services/impl/UserServiceImplTest.java
@@ -20,6 +20,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -151,7 +154,13 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWhitSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+
+        verify(repository, times(1)).deleteById(anyInt());
+
     }
 
     private void startUser() {
