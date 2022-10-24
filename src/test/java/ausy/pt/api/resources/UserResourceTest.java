10c13334b9da96adfcfae3730b7a14f356d0c314
RodneyFernandes17 <rodfelfer@hotmail.com>
2022-10-24 16:32:16 +0100

    teste para endpoint findbyId para classe user resource

--- src/test/java/ausy/pt/api/resources/UserResourceTest.java
+++ src/test/java/ausy/pt/api/resources/UserResourceTest.java
@@ -10,17 +10,21 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
 
-    public static final int INDEX = 0;
     private static final Integer ID = 1;
     private static final String NAME = "Rods";
     private static final String EMAIL = "rod@mail.com";
     private static final String PASSWORD = "123";
-    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
-    public static final String EMAIL_JA_CADASTRADO = "Email já cadastrado!";
 
     private User user;
 
@@ -42,7 +46,22 @@ class UserResourceTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdThenReturnSuccess() {
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
+
     }
 
     @Test
