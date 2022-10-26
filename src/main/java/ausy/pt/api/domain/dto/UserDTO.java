2f5c6d490e6bb6414136245e1bf153654971eed2
RodneyFernandes17 <rodfelfer@hotmail.com>
2022-10-26 11:20:47 +0100

    removendo anotacoes nao utilizadas

--- src/main/java/ausy/pt/api/domain/dto/UserDTO.java
+++ src/main/java/ausy/pt/api/domain/dto/UserDTO.java
@@ -3,12 +3,10 @@ package ausy.pt.api.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
--- src/main/java/ausy/pt/api/resources/exceptions/StandardError.java
+++ src/main/java/ausy/pt/api/resources/exceptions/StandardError.java
@@ -3,10 +3,8 @@ package ausy.pt.api.resources.exceptions;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
 
